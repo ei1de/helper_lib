c6bf27d866e43701a82e7380be34cede739f81d1
Elchyn Iskenderov <elchiniskanderov@gmail.com>
2023-08-05 19:13:13 +0300

    fixes

--- app/src/main/java/com/libhelper/lib/MainActivity.kt
+++ app/src/main/java/com/libhelper/lib/MainActivity.kt
@@ -15,23 +15,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CoroutineScope(Dispatchers.IO).launch {
-            val urlBuilder = LinkBuilder.Builder()
-                .setAfUserId(this@MainActivity.getAfUserID{Log.d("TAGG", "GET UID ERROR")})
-                .setAppsKey("CuMkMgVWpTFur6VxbZwqea")
-                .setExceptionLogger { Log.d("TAGG", "${it.message}") }
-                .setOneSignalSender { push, afUId -> Log.d("TAGG", "$push $afUId") }
-                .setRemoteData("https://trubach.cfd/stargold/","id","tok","k","dom")
-                .build()
-            val result = urlBuilder.buildUrl(this@MainActivity){
-                it.caesar()
-            }
-            when(result){
-                is BuildResult.Success->{
-                }
-                else->{
-                }
-            }
-        }
     }
 }
